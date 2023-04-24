6c86531cbd9a123a79befc758e8415d543572d9d
Amadeo de Gracia Herranz <amadeodeg@gmail.com>
2023-04-24 15:43:29 +0200

    Basic functions and copyclip

--- main.js
+++ main.js
@@ -1,3 +1,210 @@
+
+let canvas
+let canvas_context;
+let canvas_width;
+let canvas_height;
+
+let listdiv;
+
+let GRID_SIZE = 15; //px
+let MARKER_OFFSET_X = GRID_SIZE/2;
+let MARKER_OFFSET_Y = GRID_SIZE/2;
+
+let marker_list = [];
+
+let marker_selected;
+let element_selected;
+let listfield_selected;
+
 function mainOnLoad() {
-    console.log("hello world")
+    console.log("hello world");
+
+    listdiv = document.getElementById("listdivID");
+    canvas=document.getElementById("canvasID");
+    canvas_width = canvas.width;
+    canvas_height = canvas.height;
+    canvas_context = canvas.getContext("2d");
+    updateCanvas();
+}
+
+function updateCanvas() {
+    canvas_context.clearRect(0, 0, canvas_width, canvas_height);
+    drawBackground();
+    drawSpecialLines();
+    drawGrid();
+    drawMarkerLines();
+}
+
+function drawBackground(){
+    canvas_context.fillStyle = "#f3f6f4";
+    canvas_context.fillRect(0, 0, canvas_width, canvas_height);
+}
+
+function drawSpecialLines(){
+    SPECIAL_LINES_COLORS = ["#bcbcbc"];
+
+    SPECIAL_LINES_HORIZONTAL = [0, 5, 10, 15, 20, 25, 30];
+    SPECIAL_LINES_HORIZONTAL = [15]
+    for (var j =0; j<SPECIAL_LINES_HORIZONTAL.length; j+=1){
+        canvas_context.fillStyle = SPECIAL_LINES_COLORS[0];
+        canvas_context.fillRect(0,SPECIAL_LINES_HORIZONTAL[j]*GRID_SIZE, canvas_width, GRID_SIZE);
+    }
+
+    SPECIAL_LINES_VERTICAL = [0, 10, 20, 30, 40, 50, 60];
+    SPECIAL_LINES_VERTICAL =[]
+    for (var i =0; i<SPECIAL_LINES_VERTICAL.length; i+=1){
+        canvas_context.fillStyle = SPECIAL_LINES_COLORS[0];
+        canvas_context.fillRect(SPECIAL_LINES_VERTICAL[i]*GRID_SIZE,0, GRID_SIZE, canvas_height);
+    }
+
+
+}
+
+function drawGrid(){
+    canvas_context.beginPath(); // only for lineTO
+
+    for (var i=0; i<=canvas_width; i+=GRID_SIZE){
+        canvas_context.moveTo(i,0);
+        canvas_context.lineTo(i,canvas_height);
+    }
+
+    for (var j=0; j<=canvas_height; j+=GRID_SIZE){
+        canvas_context.moveTo(0,j);
+        canvas_context.lineTo(canvas_width,j);
+    }
+
+    canvas_context.strokeStyle = "#999999"
+    canvas_context.lineWidth = 1; //px
+    canvas_context.stroke();
+}
+
+function drawMarkerLines(){
+    canvas_context.beginPath();
+
+    for(var i=0; i<marker_list.length-1; i++){
+        canvas_context.moveTo(marker_list[i].x,marker_list[i].y);
+        canvas_context.lineTo(marker_list[i+1].x,marker_list[i+1].y)
+    }
+    canvas_context.strokeStyle = "red";
+    canvas_context.lineWidth =2;
+    canvas_context.stroke();
+}
+
+function canvasClick(event) {
+    marker_list.push(new Marker(event.clientX, event.clientY));
+    updateCanvas();
+}
+
+class Marker {
+    constructor(posX, posY){
+        this.x = Math.floor((posX + MARKER_OFFSET_X) /GRID_SIZE) * GRID_SIZE - MARKER_OFFSET_X;
+        this.y = Math.floor((posY + MARKER_OFFSET_Y) /GRID_SIZE) * GRID_SIZE - MARKER_OFFSET_Y;
+        this.xshown = this.x;
+        this.yshown = this.y;
+        this.updatexyshown();
+        this.element_marker = null;
+        this.element_list = null;
+        this.createElementMarker();
+        this.setPosition();
+        this.createElementList();
+        
+    }
+
+    createElementMarker() {
+        this.element_marker = document.createElement("div");
+        this.element_marker.innerHTML = 0;
+        this.element_marker.className = "marker"
+        document.body.appendChild(this.element_marker);
+        
+        this.element_marker.addEventListener("click",
+            (event)=> {
+                if(marker_selected == this.element_marker){
+                    marker_selected.classList.remove("mark_selected");
+                    marker_selected = null;
+                    element_selected.classList.remove("selected");
+                }else{
+                    if(marker_selected){
+                        marker_selected.classList.remove("mark_selected");
+                        element_selected.classList.remove("selected");
+                    }
+                    this.element_marker.classList.add("mark_selected");
+                    marker_selected = this.element_marker;
+                    this.element_list.classList.add("selected");
+                    element_selected = this.element_list;
+                }
+
+            });
+
+        this.element_marker.addEventListener("dblclick",
+            (event)=> {
+                console.log("double click")
+            });
+        this.element_marker.addEventListener("contextmenu",
+            (event)=> {
+                console.log("right click")
+            });
+    }
+
+    setPosition() {
+        this.element_marker.style.left = this.x + "px";
+        this.element_marker.style.top = this.y + "px";
+    }
+
+    createElementList() {
+        this.element_list = document.createElement("div");
+        this.element_list.className = "elementList"
+        this.element_list.innerHTML = `<div class="row">
+                                <label>idx</label>
+                                <label for="time">Time:</label>
+                                <input class="input" name="time" type="text" value="${this.xshown}">
+                                <label for="power">Power:</label>
+                                <input class="input" name="power" type="text" value="${this.yshown}">
+                                <button class="remove"> X </button>
+                            </div>`;
+        listdiv.appendChild(this.element_list);
+        let remove_button = this.element_list.getElementsByClassName("remove")[0];
+        remove_button.addEventListener("click",
+            (event)=> {
+                this.element_list.remove();
+                this.element_marker.remove();
+                marker_list.splice(marker_list.indexOf(this),1);//pop does not work
+                updateCanvas();
+            });
+        let time_input = this.element_list.getElementsByClassName("input")[0];
+        time_input.addEventListener("change",
+            (event)=> {
+                this.xshown = parseFloat(time_input.value);
+                this.updatexyabs();
+                this.setPosition();
+                updateCanvas();
+            });
+        let power_input = this.element_list.getElementsByClassName("input")[1];
+        power_input.addEventListener("change",
+            (event)=> {
+                this.yshown = parseFloat(power_input.value);
+                this.updatexyabs();
+                this.setPosition();
+                updateCanvas();
+            });
+    }
+
+    updatexyshown(){
+        this.xshown = this.x/GRID_SIZE -0.5;
+        this.yshown = this.y/GRID_SIZE -0.5;
+    }
+    updatexyabs(){
+        this.x = (this.xshown+0.5)* GRID_SIZE;
+        this.y = (this.yshown+0.5)* GRID_SIZE;
+    }
+}
+
+function copyClip(){
+    var result="hello clip\n"
+    for (var i=0; i<marker_list.length; i++){
+        var e = marker_list[i];
+        var aux= ""+e.xshown+" "+e.yshown+"\n";
+        result+=aux; 
+    }
+    navigator.clipboard.writeText(result);
+    alert("Copied")
 }
